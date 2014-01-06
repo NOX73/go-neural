026ac52c96854a7aa99d175ae6288458f4b715a9
Rozhnov Alexandr <nox73@ya.ru>
2014-01-07 01:55:01 +0400

    Persist Dump from to funcs.

--- persist/persist.go
+++ persist/persist.go
@@ -7,7 +7,7 @@ import (
 )
 
 type Weights [][][]float64
-type Network struct {
+type NetworkDump struct {
   Enters        int
   Weights   Weights
 }
@@ -16,10 +16,40 @@ func FromFile ( path string ) *neural.Network {
   b, err := ioutil.ReadFile(path)
   if err != nil { panic(err) }
 
-  nDump := &Network{}
+  nDump := &NetworkDump{}
   err = json.Unmarshal(b, nDump)
   if err != nil { panic(err) }
 
+  n := FromDump(nDump)
+  return n
+}
+
+func ToFile ( path string, n *neural.Network ) {
+  nDump := ToDump(n)
+  j, _ := json.Marshal(nDump)
+
+  err := ioutil.WriteFile(path, j, 0644)
+  if err != nil { panic(err) }
+}
+
+func ToDump ( n *neural.Network ) *NetworkDump {
+
+  nDump := &NetworkDump{Enters: len(n.Enters), Weights: make([][][]float64, len(n.Layers))}
+
+  for i, l := range n.Layers {
+    nDump.Weights[i] = make([][]float64, len(l.Neurons))
+    for j, n := range l.Neurons {
+      nDump.Weights[i][j] = make([]float64, len(n.InSynapses))
+      for k, s := range n.InSynapses {
+        nDump.Weights[i][j][k] = s.Weight
+      }
+    }
+  }
+
+  return nDump
+}
+
+func FromDump ( nDump *NetworkDump ) *neural.Network {
   layers := make([]int, len(nDump.Weights))
   for i, layer := range nDump.Weights {
     layers[i] = len(layer)
@@ -36,24 +66,4 @@ func FromFile ( path string ) *neural.Network {
   }
 
   return n
-}
-
-func ToFile ( path string, n *neural.Network ) {
-
-  nDump := Network{Enters: len(n.Enters), Weights: make([][][]float64, len(n.Layers))}
-
-  for i, l := range n.Layers {
-    nDump.Weights[i] = make([][]float64, len(l.Neurons))
-    for j, n := range l.Neurons {
-      nDump.Weights[i][j] = make([]float64, len(n.InSynapses))
-      for k, s := range n.InSynapses {
-        nDump.Weights[i][j][k] = s.Weight
-      }
-    }
-  }
-
-  j, _ := json.Marshal(nDump)
-
-  err := ioutil.WriteFile(path, j, 0644)
-  if err != nil { panic(err) }
 }
