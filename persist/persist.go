26ce70362e6f850cda9d13c4355651a756b3eabf
Rozhnov Alexandr <nox73@ya.ru>
2014-02-02 22:58:17 +0400

    Dump from to file.

--- persist/persist.go
+++ persist/persist.go
@@ -12,25 +12,35 @@ type NetworkDump struct {
 	Weights Weights
 }
 
-func FromFile(path string) *neural.Network {
+func DumpFromFile(path string) *NetworkDump {
 	b, err := ioutil.ReadFile(path)
 	if err != nil {
 		panic(err)
 	}
 
-	nDump := &NetworkDump{}
-	err = json.Unmarshal(b, nDump)
+	dump := &NetworkDump{}
+	err = json.Unmarshal(b, dump)
 	if err != nil {
 		panic(err)
 	}
 
-	n := FromDump(nDump)
+	return dump
+}
+
+func FromFile(path string) *neural.Network {
+	dump := DumpFromFile(path)
+	n := FromDump(dump)
 	return n
 }
 
 func ToFile(path string, n *neural.Network) {
-	nDump := ToDump(n)
-	j, _ := json.Marshal(nDump)
+	dump := ToDump(n)
+
+	DumpToFile(path, dump)
+}
+
+func DumpToFile(path string, dump *NetworkDump) {
+	j, _ := json.Marshal(dump)
 
 	err := ioutil.WriteFile(path, j, 0644)
 	if err != nil {
@@ -40,33 +50,33 @@ func ToFile(path string, n *neural.Network) {
 
 func ToDump(n *neural.Network) *NetworkDump {
 
-	nDump := &NetworkDump{Enters: len(n.Enters), Weights: make([][][]float64, len(n.Layers))}
+	dump := &NetworkDump{Enters: len(n.Enters), Weights: make([][][]float64, len(n.Layers))}
 
 	for i, l := range n.Layers {
-		nDump.Weights[i] = make([][]float64, len(l.Neurons))
+		dump.Weights[i] = make([][]float64, len(l.Neurons))
 		for j, n := range l.Neurons {
-			nDump.Weights[i][j] = make([]float64, len(n.InSynapses))
+			dump.Weights[i][j] = make([]float64, len(n.InSynapses))
 			for k, s := range n.InSynapses {
-				nDump.Weights[i][j][k] = s.Weight
+				dump.Weights[i][j][k] = s.Weight
 			}
 		}
 	}
 
-	return nDump
+	return dump
 }
 
-func FromDump(nDump *NetworkDump) *neural.Network {
-	layers := make([]int, len(nDump.Weights))
-	for i, layer := range nDump.Weights {
+func FromDump(dump *NetworkDump) *neural.Network {
+	layers := make([]int, len(dump.Weights))
+	for i, layer := range dump.Weights {
 		layers[i] = len(layer)
 	}
 
-	n := neural.NewNetwork(nDump.Enters, layers)
+	n := neural.NewNetwork(dump.Enters, layers)
 
 	for i, l := range n.Layers {
 		for j, n := range l.Neurons {
 			for k, s := range n.InSynapses {
-				s.Weight = nDump.Weights[i][j][k]
+				s.Weight = dump.Weights[i][j][k]
 			}
 		}
 	}
